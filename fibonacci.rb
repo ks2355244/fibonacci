eabeb85653eae33bc071eaea320258a5f51ec194
ks2355244 <ks2355244@yahoo.com>
2016-06-09 10:11:38 +0800

    1st commit

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,44 @@
+Pseudocode
+fibonacci_iterative
+1. Take N as input & return 'n'th Fibonacci number.  
+
+
+
+
 def fibonacci_iterative(n)
-end
+	array = []
+		m1 = 0
+		m2 = 1
+		array << m1
+		array << m2
+			n.times do
+				array << (array[m1] + array[m2])
+				m1 += 1
+				m2 += 1
+			end
+			array[n]
+	end
 
 def fibonacci_recursive(n)
+	if n <= 1
+			return n
+		else
+			fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+		end
 end
+
+puts fibonacci_iterative(0) == 0
+puts fibonacci_iterative(1) == 1
+puts fibonacci_iterative(2) == 1
+puts fibonacci_iterative(3) == 2
+puts fibonacci_iterative(4) == 3
+puts fibonacci_iterative(5) == 5
+puts fibonacci_iterative(14)
+	
+puts fibonacci_recursive(0) == 0
+puts fibonacci_recursive(1) == 1
+puts fibonacci_recursive(2) == 1
+puts fibonacci_recursive(3) == 2
+puts fibonacci_recursive(4) == 3
+puts fibonacci_recursive(5) == 5
+puts fibonacci_recursive(6) == 8
